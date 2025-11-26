c41e6d936a00130c85e517345b113e895ce24763
Johann Petrak <johann.petrak@gmail.com>
2025-11-26 17:33:20 +0100

    Make gendoc work temporarily The way how llms_wrapper_webchat runs the command line parser on import conflicts with how pdoc3 imports modules for documenting them.

--- llms_wrapper/__init__.py
+++ llms_wrapper/__init__.py
@@ -2,3 +2,6 @@ from loguru import logger
 from llms_wrapper.version import __version__
 logger.disable("llms_wrapper")
 
+# prevent pdoc3 from loading llms_wrapper_webchat
+__pdoc__ = {}
+__pdoc__["llms_wrapper_webchat"] = False
